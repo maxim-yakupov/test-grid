3a94d728e4941cdae65b32f54fa370cedebe63a2
Maxim Yakupov <maxim.yakupov@gmail.com>
2023-12-18 15:01:13 +0300

    init angular app

---
+++ src/app/grid/grid.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { GridComponent } from './grid.component';
+
+@NgModule({
+  declarations: [
+    GridComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+})
+export class AppModule { }
